97a52aec16987be3c2b82e2f2a754cabcade5d98
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-19 16:38:57 -0700

    sessions controller test, basic user index and show view

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "gets login form" do
+    get login_path
+    must_respond_with :success
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+
+  # index should redirect to index
+    it "can show all users by calling index" do
+      get users_path
+      must_respond_with :success
+    end
+
+    # create should change the db count
+    it "must update DB upon create" do
+      proc { post users_path, params: { user:
+            { username: "felicity"
+            }
+          }
+        }.must_change 'User.count', 1
+    end
+
+
+    # create should redirect to root
+
+    it "creating a new user should redirect to root" do
+      post users_path, user: {username: "georgianna"}
+      must_redirect_to :root
+    end
+    #
+    # show should redirect to individual user's page
+    it "show user should show one user" do
+      get user_path(users(:aurora))
+      must_respond_with :success
+    end
+
+
+
+
+
 end
