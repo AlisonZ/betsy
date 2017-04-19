16f7f0361c64ff4ef8da18d621cd53c9301aa657
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-19 16:18:46 -0700

    Added initial model testing for order_item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  validates :quantity, presence: true, numbericality: { only_integer: true, greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -3,7 +3,45 @@ require "test_helper"
 describe OrderItem do
   let(:order_item) { OrderItem.new }
 
-  it "must be valid" do
-    value(order_item).must_be :valid?
+  it "new order_item requires a quanitity" do
+    order_item.valid?.must_equal false
+    order_item.errors.messages.must_include :quantity
   end
+
+  it "quantity must be greater than 0" do
+    order_item.product_id = products(:fancy_socks).id
+    order_item.order_id = orders(:order_one).id
+    order_item.quantity = 0
+    order_item.valid?.must_equal false
+    order_item.errors.messages.must_include :quantity
+  end
+
+  it "quantity must be an integer" do
+    order_item.product_id = products(:fancy_socks).id
+    order_item.order_id = orders(:order_one).id
+    order_item.quantity = "two"
+    order_item.valid?.must_equal false
+    order_item.errors.messages.must_include :quantity
+  end
+
+  it "new order_item requires a Product" do
+    order_item.quantity = 2
+    order_item.valid?.must_equal false
+    order_item.errors.messages.must_include :product
+  end
+
+  it "new order_item requires an Order" do
+    order_item.quantity = 2
+    order_item.product_id = products(:fancy_socks).id
+    order_item.valid?.must_equal false
+    order_item.errors.messages.must_include :order
+  end
+
+  it "can create a new order item" do
+    order_item.quantity = 2
+    order_item.product_id = products(:fancy_socks).id
+    order_item.order_id = orders(:order_one).id
+    order_item.save.must_equal true
+  end
+
 end
