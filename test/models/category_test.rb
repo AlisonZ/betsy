e57f3f7ba3fa32bae58861f45f987effd701d205
AlisonZ <alisonkzerbe@gmail.com>
2017-04-19 16:13:35 -0700

    model validations for category, pre-product relationship

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,36 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+    let(:category) { Category.new }
 
-  it "must be valid" do
-    value(category).must_be :valid?
-  end
+    #these could all be dried up a lot with fixtures
+
+    it "is valid with a name" do
+        category = Category.new
+        category.name = "nonfiction"
+        result = category.valid?
+        result.must_equal true
+    end
+
+    it "is invalid without a name" do
+        category = Category.new
+        result = category.valid?
+        result.must_equal false
+        category.errors.messages.must_include :name
+    end
+
+    it "is invalid if a category is not unique" do
+        category_one = Category.new
+        category_one.name = "fiction"
+        category_one.save
+
+        category_two = Category.new
+        category_two.name = "fiction"
+        category_two.save
+
+        result = category_two.valid?
+        result.must_equal false
+        # category.errors.messages.must_include :name
+    end
+
 end
