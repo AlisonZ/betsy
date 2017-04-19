05c6915d027e9ce61590e02221624a955748f86f
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-19 16:28:25 -0700

    created index, show, new, create, edit, update, routes and controller actions, view pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  # belongs_to :user
+  # has_many_and_belongs_to :categories
+  # has_many :reviews
+  # has_many :order_items
+
+  validates :name, presence: true
+  # validates :user_id, presence: true
+  validates :price, presence: true
+
+
+
 end
