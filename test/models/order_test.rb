033a4c2ed228bbdcf5f43f528c10f63f77a6042b
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-19 16:46:15 -0700

    Added initial tests for Order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,7 +3,15 @@ require "test_helper"
 describe Order do
   let(:order) { Order.new }
 
-  it "must be valid" do
-    value(order).must_be :valid?
+  it "must have at least one order_item" do
+    order.valid?.must_equal false
+    order.errors.messages.must_include :order_items
+  end
+
+  it "can be created with an order_item" do
+    order.order_items = [order_items(:socks)]
+    #order items fixture has to be wrapped in an array
+    #to_do maybe fix the fixtures?
+    order.valid?.must_equal true
   end
 end
