e4a9c462fda672dc9e9b09ccbb2a37e654ad8c64
AlisonZ <alisonkzerbe@gmail.com>
2017-04-20 10:11:40 -0700

    reviews model tests and validations are finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
     validates :product_id, presence: true
-    validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
+    validates :rating, numericality: { allow_nil: true, only_integer: true, greater_than: 0, less_than: 6 }
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -18,7 +18,25 @@ describe Review do
   end
 
   it "is valid with a rating" do
-
+      review = Review.new
+      review.product_id = 1
+      review.rating = 2
+      review.rating.must_be_instance_of Integer
   end
 
+  it "rating is invalid if beyond 1-5" do
+      review = Review.new
+      review.product_id = 2
+      review.rating = 9
+      result = review.valid?
+      result.must_equal false
+      review.errors.messages.must_include :rating
+  end
+
+  it "is a valid review without a rating" do
+      review = Review.new
+      review.product_id = 3
+      result = review.valid?
+      result.must_equal true
+  end
 end
