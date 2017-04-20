131de05507e890b474dd5bdcc249f1cdedf13206
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-19 17:08:51 -0700

    added a couple products fixtures and tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,15 @@ describe ProductsController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  it "should get index" do
+      get products_path
+      must_respond_with :success
+    end
+
+  it "should show one product" do
+      get products_path(products(:umbrella).id)
+      must_respond_with :success
+    end
+
 end
