9e8e6c68dfecd8477521ee2dcb4b86e9dabb445e
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-20 15:45:49 -0700

    product reviews now show up on product show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,9 +8,10 @@ class ReviewsController < ApplicationController
         @review.title = params[:review][:title]
         @review.rating = params[:review][:rating]
         @review.product_id = params[:product_id]
+        @review.review_text = params[:review][:review_text]
         if @review.save
 
-            redirect_to products_path
+            redirect_to product_path(params[:product_id])
         else
             render 'new'
         end
