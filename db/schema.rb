5488c9e4d1a37fa1b5dac97a495e99e9f05e31d0
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-20 16:31:11 -0700

    Added required:true in form_for on checkout page to make sure user fills out everything!

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170420224938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,3 +79,5 @@
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+end
