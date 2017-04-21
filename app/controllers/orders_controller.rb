395d8cbdc73ee01c69bd38cb3f7c96a7520f52f7
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-21 12:04:15 -0700

    Testing orders controller WIP

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
   def checkout
     @order = Order.find_by_id(session[:order_id])
+    if @order.nil?
+      flash[:warning] = "Please add an item to your cart to check out"
+      redirect_to root_path
+    end
   end
 end
