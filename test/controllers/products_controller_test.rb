55c9f650ee96ba35fdef76d5e38b218442090b80
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-24 14:40:47 -0700

    Changed product fixtures to have user field instead of user_id

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,18 +1,18 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
 
-  it "should get index" do
-      get products_path
-      must_respond_with :success
-    end
+  describe "#Index" do
+    it "should get index" do
+        get products_path
+        must_respond_with :success
+      end
+  end
 
-  it "should show one product" do
-      get products_path(products(:umbrella).id)
-      must_respond_with :success
-    end
-
+  describe "#Show" do
+    it "should show one product" do
+        get product_path(products(:umbrella).id)
+        must_respond_with :success
+      end
+  end
 end
