3d6f2d60da31482bde1b54fe141d9ffdbbc2fec4
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-24 16:12:13 -0700

    added user permissions for add category button from categories page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,13 @@ class SessionsController < ApplicationController
     if @user
       session[:user_id] = @user.id
       flash[:success] = "Successfully logged in as existing user #{@user.username}"
-      redirect_to users_path
-      # redirect_to :root
+      redirect_to :root
     else
       @user = User.create(username: params[:username], email: params[:email])
       if @user.id != nil
         session[:user_id] = @user.id
         flash[:success] = "Successfully created new user #{@user.username} with ID #{@user.id}"
-        redirect_to users_path
-        # redirect_to :root
+        redirect_to :root
       else
         flash.now[:failure] = "Sorry, unable to log in at this time"
         render :login_form
@@ -27,8 +25,7 @@ class SessionsController < ApplicationController
 
   def logout
     session[:user_id] = nil
-    redirect_to users_path
-    # redirect_to :root
+    redirect_to :root
 
   end
 
