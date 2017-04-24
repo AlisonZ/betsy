23c71ef100098f0dc6b3f90a12c9c91c40d9aae5
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-24 14:12:02 -0700

    Finally able to access update order status

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -38,11 +38,18 @@ describe OrdersController do
     end
 
     it "Updates the status of order to 'Paid'" do
+      # Initialize the cart by adding an item to it
       post new_order_item_path(products(:fancy_socks).id),
         params: {order_item:
           {quantity:   (order_items(:socks).quantity)}
         }
-      put order_path(orders(:order_one).id), params: {order:
+
+      # Get the newly created order (should be the last row in the Order table)
+      order = Order.last
+
+      old_status = order.status
+
+      put order_path(order.id), params: {order:
         {email: "lynn@gmail.com",
          name_on_cc: "Lynn Trickey",
          cc_number: 123,
@@ -50,15 +57,39 @@ describe OrdersController do
          billing_zip: 123,
          address: "123 Avenue St.",
          }}
-      updated_order = Order.find_by_id(orders(:order_one).id)
-      new_status = updated_order.status
-      old_status = orders(:order_one).status
+      order = Order.find(order.id)
+      order.status.must_equal "Paid"
+      new_status = order.status
 
-      new_status.must_not_equal old_status
+      new_status.wont_equal old_status
+    end
+
+    it "Updates the status of order to 'Paid'" do
+      # Initialize the cart by adding an item to it
+      post new_order_item_path(products(:fancy_socks).id),
+        params: {order_item:
+          {quantity:   (order_items(:socks).quantity)}
+        }
+
+      # Get the newly created order (should be the last row in the Order table)
+      order = Order.last
+
+      old_status = order.status
+
+      put order_path(order.id), params: {order:
+        {email: "lynn@gmail.com",
+         name_on_cc: "Lynn Trickey",
+         cc_number: 123,
+         cc_ccv: 123,
+         billing_zip: 123,
+         address: "123 Avenue St.",
+         }}
+      order = Order.find(order.id)
+      order.status.must_equal "Paid"
+      new_status = order.status
+
+      new_status.wont_equal old_status
     end
   end
 
-  #Need to write a test for if it doesn't work - then what!
-
-  #
 end
