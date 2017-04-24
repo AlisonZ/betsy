e3db40b5d524acf5e4e8b24678f4fe7cfa9321dc
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-24 14:59:40 -0700

    Added a lot of products controller tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -32,4 +32,78 @@ describe ProductsController do
         must_respond_with :missing
     end
   end
+
+  describe "#New" do
+    it "should get a form for a new product" do
+      get new_product_path
+      must_respond_with :success
+    end
+  end
+
+  describe "#Create" do
+    it "should redirect to index after creating a new product" do
+      post products_path params: { product: {
+        name: "My new Product",
+        user_id: users(:felix).id,
+        description: "This is great!",
+        stock: 123,
+        price: 12400.00,
+        photo_url: "www.url.com",
+        selling_status: true,
+        category_ids: [categories(:fiction).id, categories(:nonfiction).id]
+      } }
+      must_respond_with :redirect
+      must_redirect_to products_path
+    end
+
+    it "should update model after creating a new product" do
+      proc { post products_path params: { product: {
+        name: "My new Product",
+        user_id: users(:felix).id,
+        description: "This is great!",
+        stock: 123,
+        price: 12400.00,
+        photo_url: "www.url.com",
+        selling_status: true,
+        category_ids: [categories(:fiction).id, categories(:nonfiction).id]
+      } } }.must_change 'Product.count', 1
+    end
+
+    it "should not update model with bad data" do
+      proc { post products_path params: { product: {
+        name: "",
+        user_id: 0,
+        description: "This is great!",
+        stock: 123,
+        price: 00,
+        photo_url: "www.url.com",
+        selling_status: true,
+        category_ids: []
+      } } }.must_change 'Product.count', 0
+    end
+  end
+
+  describe "#Edit" do
+    it "should get a form to edit a product" do
+      get edit_product_path(products(:umbrella))
+      must_respond_with :success
+    end
+  end
+
+  describe "#Update" do
+    it "should redirect to show page after creating a new product" do
+      put product_path(products(:umbrella).id), params: { product: {
+        name: "Umbrella",
+        user_id: users(:aurora).id,
+        description: "This is great!",
+        stock: 123,
+        price: 12400.00,
+        photo_url: "www.url.com",
+        selling_status: true,
+        category_ids: [categories(:fiction).id, categories(:nonfiction).id]
+      } }
+      must_respond_with :redirect
+      must_redirect_to product_path(products(:umbrella).id)
+    end
+  end
 end
