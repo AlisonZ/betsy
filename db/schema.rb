4bd0f376a3906fbb83c55d71f9ef786e75f5cb61
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-25 11:24:38 -0700

    Fixed credit card number limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425161810) do
+ActiveRecord::Schema.define(version: 20170425181535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20170425161810) do
     t.string   "email"
     t.string   "address"
     t.string   "name_on_cc"
-    t.integer  "cc_number"
+    t.bigint   "cc_number"
     t.integer  "cc_ccv"
     t.integer  "billing_zip"
   end
