85baa60a12ee0777b97fe8af11f2a268de31d78e
AlisonZ <alisonkzerbe@gmail.com>
2017-04-26 16:39:06 -0700

    users controller testing working for oauth

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,62 +1,61 @@
 require "test_helper"
 
 describe UsersController do
+    describe "a not logged in user" do
+        # index should redirect to index
 
+        it "can show all users by calling index" do
+            get users_path
+            must_respond_with :success
+        end
 
-  # index should redirect to index
-    it "can show all users by calling index" do
-      get users_path
-      must_respond_with :success
+        it "succeeds with many users" do
+            # Assumption: there are many users in the DB
+            User.count.must_be :>, 0
+
+            get users_path
+            must_respond_with :success
+        end
+
+        it "succeeds with no users" do
+            # Start with a clean slate
+            User.destroy_all
+
+            get users_path
+            must_respond_with :success
+        end
     end
 
-    it "succeeds with many users" do
-      # Assumption: there are many users in the DB
-      User.count.must_be :>, 0
+    describe "a user is logged in " do
+        before do
+            login_user(users(:felix))
+        end
+        # show should redirect to individual user's page
+        it "#show should show one user" do
+            get user_path(users(:felix).id)
+            must_respond_with :success
+        end
 
-      get users_path
-      must_respond_with :success
+        it "should get edit form for profile" do
+            get edit_user_path(users(:felix).id)
+            must_respond_with :success
+        end
+
+        it "won't show if not your account" do
+            get user_path(users(:aurora).id)
+            must_redirect_to :root
+        end
     end
-
-    it "succeeds with no users" do
-      # Start with a clean slate
-      User.destroy_all
-
-      get users_path
-      must_respond_with :success
-    end
-
-    #
-    # show should redirect to individual user's page
-    it "show user should show one user" do
-      get auth_github_callback_path, params: { provider: "github", uid: "12345" }
-      test_user = User.where(uid: "12345")
-      get user_path(test_user.id)
-      must_respond_with :success
-    end
-
-    it "won't show if not your account" do
-      get user_path(users(:felix).id)
-      must_redirect_to :root
-    end
-
-    # This is obsolete because of the check_owner method in ApplicationController
-
-    # it "renders 404 not_found for a bogus user" do
-    #   # User.last gives the user with the highest ID
-    #   # bogus_user_id = User.last.id + 1
-    #   get user_path("fake_id")
-    #   must_redirect_to "/public/404.html"
-    # end
-
-
-    describe "#edit" do
-      it "should get edit form for profile" do
-        get edit_user_path(users(:aurora).id)
-        must_respond_with :success
-      end
-    end
-
-
-
-
 end
+
+
+
+
+# This is obsolete because of the check_owner method in ApplicationController
+
+# it "renders 404 not_found for a bogus user" do
+#   # User.last gives the user with the highest ID
+#   # bogus_user_id = User.last.id + 1
+#   get user_path("fake_id")
+#   must_redirect_to "/public/404.html"
+# end
