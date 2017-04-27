55dd455efe908a78653a9a75b729a5f4cca75d8c
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-27 15:51:03 -0700

    added force ssl to production rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,7 +20,7 @@ class OrdersController < ApplicationController
   def checkout
     @order = Order.find_by_id(session[:order_id])
     if @order.nil? || @order.order_items == []
-      flash[:warning] = "Please add an item to your cart to check out"
+      flash[:failure] = "Please add an item to your cart to check out"
       redirect_to root_path
     end
   end
