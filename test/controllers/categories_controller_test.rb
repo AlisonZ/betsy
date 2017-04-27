fec74ac9182f11af5be93b14ee20ca95f4eb9d2b
AlisonZ <alisonkzerbe@gmail.com>
2017-04-26 20:50:29 -0700

    categories controller tests working and logged in vs not logged in functionality is separated

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,30 +3,32 @@ require "test_helper"
 #copied this line from the books_app controller test
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
     describe CategoriesController do
-        #wrap in logged in 
-        before do
-            login_user(users(:felix))
-        end
+        #wrap in logged in
+        describe "a user is logged in" do
+            before do
+                login_user(users(:felix))
+            end
+            it "should show the new form" do
+                get new_category_path
+                must_respond_with :success
+            end
 
-        it "should show the new form" do
-            get new_category_path
-            must_respond_with :success
-        end
-
-        it "can create a new category" do
-            get auth_github_callback_path
-            post categories_path, params: { category: {name: "funky"}}
-            must_respond_with :redirect
-            must_redirect_to user_path(users(:felix).id)
-        end
-
-
-        it "should affect the model when creating a category" do
-            proc {
+            it "can create a new category" do
                 get auth_github_callback_path
-                post categories_path, params: { category: {name: "sci-fi"}}
-            }.must_change 'Category.count', 1
+                post categories_path, params: { category: {name: "funky"}}
+                must_respond_with :redirect
+                must_redirect_to user_path(users(:felix).id)
+            end
+
+
+            it "should affect the model when creating a category" do
+                proc {
+                    get auth_github_callback_path
+                    post categories_path, params: { category: {name: "sci-fi"}}
+                }.must_change 'Category.count', 1
+            end
         end
+
 
         describe "a user is not logged in" do
             it "should get index" do
