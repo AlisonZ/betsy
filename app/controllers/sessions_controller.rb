a3b9445fa79c0ead1e87c7c24cc2e9711612b462
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-27 14:20:38 -0700

    trying fix on heroku oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,20 +22,20 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    if auth_hash['uid']
-      user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    # if auth_hash['uid']
+    user = User.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if user.nil?
+      user = User.create_from_github(auth_hash)
       if user.nil?
-        user = User.create_from_github(auth_hash)
-        if user.nil?
-          flash[:error] = "Could not log in"
-        end
-      else
-        flash[:success] = "Successfully logged in as existing user #{user.username}"
+        flash[:error] = "Could not log in"
+        redirect_to :root
       end
-      session[:user_id] = user.id
-    else
-      flash[:error] = 'Could not log in'
     end
+    flash[:success] = "Successfully logged in as existing user #{user.id}"
+    session[:user_id] = user.id
+    # else
+    #   flash[:error] = 'Could not log in'
+    # end
     redirect_to :root
   end
 
