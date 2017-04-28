16773536f8e55513dcd82effc4ca78f6e8802a55
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-28 06:39:23 -0700

    removed flash message upon login - redundant

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,5 @@
 class SessionsController < ApplicationController
-  # def create
-  #   auth_hash = request.env['omniauth.auth']
-  #   if auth_hash['uid']
-  #     user = User.find_by(uid: auth_hash[:uid], provider: 'github')
-  #     if user.nil?
-  #       user = User.create_from_github(auth_hash)
-  #       if user.nil?
-  #         flash[:error] = "Could not log in"
-  #         redirect_to :root
-  #       end
-  #     else
-  #       flash[:success] = "Successfully logged in as existing user #{user.username}"
-  #       redirect_to :root
-  #     end
-  #     session[:user_id] = user.id
-  #   else
-  #     flash[:error] = 'Could not log in'
-  #     redirect_to :root
-  #   end
-  # end
+
 
   def create
     auth_hash = request.env['omniauth.auth']
@@ -31,7 +12,6 @@ class SessionsController < ApplicationController
         redirect_to :root
       end
     end
-    flash[:success] = "Successfully logged in as existing user #{user.id}"
     session[:user_id] = user.id
     # else
     #   flash[:error] = 'Could not log in'
